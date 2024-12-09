7fb7ad4f368a1462f7d56b11bf2d8f0e52fc8623
Ayushyaduwanshi1 <ayush1010yadav2020@gmail.com>
2024-12-09 17:04:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let userinput = document.getElementById("input");
+let button = document.getElementById("button")
+let output = document.getElementById("root2")
+function calculateAge() {
+    let birthdayValue = userinput.value;
+    let currentDate = new Date();
+    let userdate = new Date(birthdayValue);
+    let year = currentDate.getFullYear() - userdate.getFullYear();
+    let month = currentDate.getMonth() - userdate.getMonth();
+    let day = year*365
+    let totalbornday = day+(month*30)
+    if (birthdayValue === "") {
+        alert("Please enter your birthday");
+        return
+    }
+    if (year <= -1 ) {
+        alert("Enter Correct age")
+        return
+    }
+    output.innerHTML = `Your age is ${year} years <br> ${month} Month old  <br> <br> And <br><br> <br> ${totalbornday} Day`;
+    userinput.value =""
+}
+button.addEventListener("click", calculateAge)
+window.addEventListener("keypress",(e)=>{
+    if(e.key === "Enter"){
+        calculateAge()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function my(birthdayValue) {
+//     let currentDate = new Date();
+//     let userdate = new Date(birthdayValue);
+//     let year = currentDate.getFullYear() - userdate.getFullYear();
+//     let month = currentDate.getMonth() - userdate.getMonth();
+//     if (
+//         month < 0 ||
+//         (month === 0 && currentDate.getDate() < userdate.getDate())
+//     ) {
+//         year--;
+//     }
+//     return year;
+// }
+
+
